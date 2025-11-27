c8c5aef9c39adeccf033282473da571e4bdc1454
Dilara <dilara.schulze@gmail.com>
2025-11-27 13:38:48 +0100

    did stuff

--- tunnel-whisper.py
+++ tunnel-whisper.py
@@ -4,13 +4,14 @@ import ephem
 from datetime import datetime
 
 url_whisper = "http://localhost:8000/v1/audio/transcriptions"
-files = {"file": open("./audio.mp3", "rb")}
+files = {"file": open("./audio/Mars_eng.m4a", "rb")}
 data = {"model": "whisper-1"}
 
 url_ollama = "http://localhost:18080/api/chat"
 
 text = requests.post(url_whisper, files=files, data=data)
 print(text.json())
+
 
 def seek(skyobject, objtype):
     if objtype == "Star":
@@ -27,8 +28,8 @@ def seek(skyobject, objtype):
 
 messages = [
     {
-        "role": "system",
-	"content": "You are an astronomy assistant. You will be given an input and its your job to analyze, what sky object the user wants to see and what type of object, i.e. star, planet or moon it is. Return your response in the format of 'Object,Type'. Make sure you reply in English"
+    "role": "system",
+	"content": "You will be given an input and translate it into english if necessary. You will return a response in the format 'Object,Type'. The Input will include a astronomical object that someone wants to see. Return the english common name of the object for the first parameter 'Object'. For the parameter 'Type' the options are: 'Star', 'Planet', 'Moon' or 'Satellite'. Choose the correct type for the object you determined. Make sure that your response is in english and never a full sentence. For example, if the input is 'Zeige mir den Stern Sirius', your response should be 'Sirius,Star'. If the input is 'Ich möchte den Polarstern sehen', your response should be 'Polaris,Star'."
     },
     {
 	"role": "user",
@@ -52,7 +53,7 @@ print(f"Output Ollama: {output}")
 
 skyo = f"{output}"
 skyo = skyo.replace("\n", "").split(",")
-skyobj = skyo[0]
-skytyp = skyo[1]
+skyobj = skyo[0].strip()
+skytyp = skyo[1].strip()
 print(f"Skyobj: {skyobj}, Skytyp: {skytyp}")
 seek(skyobj, skytyp)
