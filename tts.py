0fcb10f2299634907cd87735cbfa0ae630e9d8fd
fasihu1996 <fasih1996@gmail.com>
2026-01-11 23:35:30 +0100

    switch TTS to German

--- tts.py
+++ tts.py
@@ -1,5 +1,9 @@
-from yapper import Yapper
-yapper = Yapper()
+from yapper import Yapper, PiperSpeaker, PiperVoiceGermany
+
+deutsch = PiperSpeaker(
+    voice=PiperVoiceGermany.EVA_K
+)
+yapper = Yapper(speaker=deutsch)
 
 def say(text: str):
     """Basic wrapper function to output TTS messages."""
